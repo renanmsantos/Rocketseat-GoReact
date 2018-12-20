7d4ba3e2c391b6789cea68356d6456f2ef7059bc
Renan Moreira Santos <renanmoreirasan@gmail.com>
2018-12-19 23:12:04 -0200

    First commit

---
+++ module-1/src/index.js
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { render } from "react-dom";
+
+class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+
+  render() {
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
+  }
+}
+
+class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Rocket!</h1>
+        <h2>{this.state.counter}</h2>
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
