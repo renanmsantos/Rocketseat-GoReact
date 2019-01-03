590b767aaba2b3902cad9bb8175f006eec723b1c
Renan Moreira Santos <renanmoreirasan@gmail.com>
2019-01-03 16:46:22 -0200

    Fixed error to load scss files.

--- module-1/webpack.config.js
+++ module-1/webpack.config.js
@@ -23,8 +23,12 @@ module.exports = {
         use: [
           {
             loader: 'style-loader',
-            loader: 'sass-loader',
+          },
+          {
             loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
           },
         ],
       },
