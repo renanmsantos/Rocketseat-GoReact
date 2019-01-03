96f9260539c0ca18af3cf70ac7e32f1f2b62130b
Renan Moreira Santos <renanmoreirasan@gmail.com>
2019-01-03 21:17:17 -0200

    Initial module 2 project.

---
+++ module2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #f00;
+  font-size: 32px;
+`;
+
+const App = () => (
+  <div className="App">
+    <Title>Teste</Title>
+  </div>
+);
+
+export default App;
